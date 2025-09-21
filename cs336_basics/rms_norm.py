ffe6fe9e93c467b0eaf52f24ea5558b6e7723a6d
wd <wd@matelink.ai>
2025-09-21 20:55:31 +0800

    finish RMSNorm

---
+++ cs336_basics/rms_norm.py
@@ -0,0 +1,22 @@
+import torch
+
+class RMSNorm(torch.nn.Module):
+
+    def __init__(self, 
+                 d_model: int, 
+                 eps: float = 1e-5, 
+                 device: torch.device | None = None, 
+                 dtype: torch.dtype | None = None):
+        super().__init__()
+
+        self.g = torch.nn.Parameter(torch.empty(d_model))
+
+        self.d_model = d_model
+        self.eps = eps
+
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        in_dtype = x.dtype
+        x = x.to(torch.float32)
+        x = x * self.g / (x.square().mean(dim=-1, keepdim=True) + self.eps).sqrt()
+        return x.to(in_dtype)
