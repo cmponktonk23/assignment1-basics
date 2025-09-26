1c51c578b84c1875662a33129fee2f5241882ddf
wd <wd@matelink.ai>
2025-09-26 22:29:50 +0800

    add comment

--- cs336_basics/train/gradient_clipping.py
+++ cs336_basics/train/gradient_clipping.py
@@ -3,8 +3,10 @@ from typing import Iterable
 
 
 def gradient_clipping(parameters: Iterable[torch.nn.Parameter], max_l2_norm: float):
+    # Calculate global l2-norm(g) = sqrt(g1^2 + g2^2 + ... + gn^2) 
     g = torch.cat([p.grad for p in parameters if p.grad is not None])
     g_norm2 = torch.norm(g, p=2)
+
     if g_norm2 >= max_l2_norm:
         for p in parameters:
             if p.grad is not None:
